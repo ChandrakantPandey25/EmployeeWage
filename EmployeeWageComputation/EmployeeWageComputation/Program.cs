2dbb466bdd6394276b2044884d77a175f62c4776
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-11 23:09:00 -0700

    Added Employee Attendance

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Compuatation");
+            Console.WriteLine("Enter 1: To check Employee Attendance");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    EmpAttendance.CheckEmployeePresentorAbsent();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Number");
+                    break;
+            }
+        }
+    }
+}
