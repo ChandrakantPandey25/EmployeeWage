6591b0d964523254a6b82ed67b4e1119e879bb70
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-11 23:27:29 -0700

    Added Employee Parttime Daily Wage

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmpPartTimeWage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmpPartTimeWage
+    {
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int EMP_RATE_PER_HR = 20;
+
+        public  int EmplPartimeWage()
+        {
+
+
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                int empHr = 8;
+                empWage = empHr * EMP_RATE_PER_HR;
+
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                int empHr = 4;
+                empWage = empHr * EMP_RATE_PER_HR;
+
+            }
+            else
+            {
+                int empHr = 0;
+                empWage = empHr * EMP_RATE_PER_HR;
+            }
+
+            return empWage;
+        }
+    }
+}
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -8,12 +8,25 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome to Employee Wage Compuatation");
             Console.WriteLine("Enter 1: To check Employee Attendance");
+            Console.WriteLine("Enter 2: To check Employee Daily Wage");
+            Console.WriteLine("Enter 3: To check Employee Parttime Daily Wage");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     EmpAttendance.CheckEmployeePresentorAbsent();
                     break;
+                case 2:
+                    EmployeeWage wage = new EmployeeWage();
+                    wage.EmpDailyWage();
+                    break;
+                case 3:
+                    EmpPartTimeWage WageParttime = new EmpPartTimeWage();
+                    WageParttime.EmplPartimeWage();
+                    break;
+
+
+
                 default:
                     Console.WriteLine("Enter Valid Number");
                     break;
