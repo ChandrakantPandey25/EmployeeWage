a2567b203d486f43cb0221057524332f0fe2d875
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-11 23:21:44 -0700

    Added Employee Daily Wage

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWage
+    {
+        public int EmpDailyWage()
+        {
+            int IS_FULL_TIME = 1, empWage = 0;
+            int RATE_PER_HR = 20;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                int empHr = 8;
+                empWage = empHr * RATE_PER_HR;
+
+            }
+            else
+            {
+                int empHr = 0;
+                empWage = empHr * RATE_PER_HR;
+
+            }
+            Console.WriteLine("Employee Daily Wage is :" + empWage);
+            return empWage;
+        }
+    }
+}
